9b9ae6cb376cede43f7b878df4fe8e3649d5065d
Aaron van den Berg <a.vdberg98@gmail.com>
2021-09-03 16:09:30 +0200

    Added Plausible Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import PlausibleProvider from 'next-plausible'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <PlausibleProvider
+            domain="next-tailwind.aaron-dev.nl"
+            customDomain="analytics.aaron-dev.nl"
+            selfHosted={true}
+            trackOutboundLinks={true}
+        >
+            <Component {...pageProps} />
+        </PlausibleProvider>
+    );
 }
 
 export default MyApp
