aac0ee24d05e1e76bdee7d2a2043b84787e7d454
Aaron van den Berg <a.vdberg98@gmail.com>
2021-09-10 17:39:03 +0200

    Fixed containerized MongoDB test

--- components/UsersList.jsx
+++ components/UsersList.jsx
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 export default function UsersList({users}) {
     return (
         <div className="flex flex-col">
@@ -43,7 +42,9 @@ export default function UsersList({users}) {
                                     <td className="px-6 py-4 whitespace-nowrap">
                                         <div className="flex items-center">
                                             <div className="flex-shrink-0 h-10 w-10">
-                                                <img className="h-10 w-10 rounded-full" src={user.image} alt="" />
+                                                <img className="h-10 w-10 rounded-full"
+                                                     src={`https://avatars.dicebear.com/api/male/${user._id}.svg`}
+                                                     alt=""/>
                                             </div>
                                             <div className="ml-4">
                                                 <div className="text-sm font-medium text-gray-900">{user.name}</div>
@@ -56,9 +57,10 @@ export default function UsersList({users}) {
                                         <div className="text-sm text-gray-500">{user.department}</div>
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap">
-                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
+                                      <span
+                                          className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                        Active
+                                      </span>
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.role}</td>
                                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
