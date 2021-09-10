489c9a4a19a8a6d75e336e43ca119e6a9e001980
Aaron van den Berg <a.vdberg98@gmail.com>
2021-09-10 17:18:57 +0200

    Removed Plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,8 @@
-import PlausibleProvider from 'next-plausible'
 import 'tailwindcss/tailwind.css'
-import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
     return (
-        <PlausibleProvider
-            domain="next-tailwind.aaron-dev.nl"
-            customDomain="https://analytics.aaron-dev.nl"
-            selfHosted={true}
-            trackOutboundLinks={true}
-        >
-            <Head>
-                <script async defer data-domain="next-tailwind.aaron-dev.nl" src="https://analytics.aaron-dev.nl/js/plausible.js"/>
-                <script defer data-domain="next-tailwind.aaron-dev.nl" src="/js/script.js"/>
-            </Head>
-            <Component {...pageProps} />
-        </PlausibleProvider>
+        <Component {...pageProps} />
     );
 }
 
